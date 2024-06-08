9b78655c213e32b8b7085e469b6f30e8b495a1c7
eric <ericmaina11455@gmail.com>
2024-06-08 15:49:09 +0300

    commenting and uncommenting

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,29 @@
 class Band:
     def __init__(self, name, hometown):
-        self.name = name
-        self.hometown = hometown
+        self._name = name
+        self._hometown = hometown
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        if isinstance(value,str) and len(value)>0:
+            self._name=value
+        else:
+            raise ValueError ("The name must not be an empty string")
+        
+    @property
+    def hometown(self):
+        return self._hometown
+    
+    @hometown.setter
+    def hometown(self,value):
+        if isinstance(value,str) and len(value)>0:
+            self._hometown=value
+        else:
+            raise ValueError("The hometown must not be an empty string")
 
     def concerts(self):
         pass
@@ -31,8 +53,30 @@ class Concert:
 
 class Venue:
     def __init__(self, name, city):
-        self.name = name
-        self.city = city
+        self._name = name
+        self._city = city
+    
+    @property
+    def venue_name(self):
+        return self._name
+    
+    @venue_name.setter
+    def venue_name(self,value):
+        if isinstance(value,str) and len(value) >0:
+            self._name=value
+        else:
+            raise ValueError("Venue name must not be an empty string")
+        
+    @property
+    def city(self):
+        return self._city
+    
+    @city.setter
+    def city(self,value):
+        if isinstance(value,str) and len(value)>0:
+            self._city=value
+        else:
+            raise ValueError("City name must not be an empty string")
 
     def concerts(self):
         pass
